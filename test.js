2c3f11fca2d7cd7b3cae7f2a89b0ebe341151a8a
Lucila Werner <lulikwerner@gmail.com>
2023-04-21 13:44:50 -0400

    deleted file

--- test.js
+++
@@ -1,8 +0,0 @@
-import ProductManager from './managers/productManager.js';
-
-const productManager = new ProductManager();
-
-(async () => {
-  const products = await productManager.getProducts();
-  console.log(products);
-})();
